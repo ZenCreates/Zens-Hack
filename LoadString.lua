ff2d6b6ea57e3d21dc06b04c61b4b987ae585888
ZenDev <76600019+ZenCreates@users.noreply.github.com>
2023-04-07 23:31:58 -0700

    Update LoadString.lua

--- LoadString.lua
+++ LoadString.lua
@@ -2,4 +2,4 @@ pcall(function()
     game.Players.LocalPlayer.PlayerGui.ZensUi:Destroy()
 end)
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ZenCreates/Zens-Hack/main/ZensHack.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZenCreates/Zens-Hack/main/ZensHack.txt"))()
