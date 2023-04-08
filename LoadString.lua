8e142ab901702656117c7f940fb8cdb073f16dd2
ZenDev <76600019+ZenCreates@users.noreply.github.com>
2023-04-07 22:50:07 -0700

    Add files via upload

---
+++ LoadString.lua
@@ -0,0 +1,5 @@
+pcall(function()
+    game.Players.LocalPlayer.PlayerGui.ZensUi:Destroy()
+end)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZenCreates/Zens-Hack/main/ZensHack.lua"))()
